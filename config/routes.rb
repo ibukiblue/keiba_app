be67ffc6dc32edfb216cda7b85ff1b5682f78088
山本息吹樹 <ibuki@yamamotoibukijunoMacBook-Pro.local>
2018-12-22 18:23:26 +0900

    no-change

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resource :user, only: %i(show), path: 'profile'
   devise_for :users
   resources :logs, only: %i(create new edit show destroy update)
+  resources :boards, only: %i(index new create show edit update destroy)
 end
